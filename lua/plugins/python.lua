cbc20fa55079b560d8973d3ea1d05338f7d5831c
J R Dunsmore <James@Dunsmore.xyz>
2026-04-10 15:03:13 +0000

    add python.nvim

---
+++ lua/plugins/python.lua
@@ -0,0 +1,17 @@
+return {
+  ---@module 'python'
+  {
+    "joshzcold/python.nvim",
+    dependencies = {
+        { "mfussenegger/nvim-dap" },
+        { "mfussenegger/nvim-dap-python" },
+        { "neovim/nvim-lspconfig" },
+        { "L3MON4D3/LuaSnip" },
+        { "nvim-neotest/neotest" },
+        { "nvim-neotest/neotest-python" },
+    },
+    ---@type python.Config
+    opts = { ---@diagnostic disable-line: missing-fields`
+    }
+  }
+}
